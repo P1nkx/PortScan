0c9e3c0033051834731046da9f6d33ed1fa97f50
P1nkx <p1nkx81@gmail.com>
2023-07-11 21:54:38 +0200

    Primer commit

---
+++ PortScan.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+## Funcions
+
+# Ctrl+C
+function ctrl_c(){
+    echo -e "\n[+] Sortint ..."
+    exit 1
+}
+function comprovarPort(){
+    (exec 3<> /dev/tcp/$1/$2) 2>/dev/null
+    
+    if [ $? -eq 0 ];then
+        echo -e "\n[+] El port $2 [OPEN]"
+    fi
+
+    exec 3<&-
+    exec 3>&-
+}
+
+# Inici
+trap ctrl_c INT
+declare -a ports=($(seq 1 65635))
+
+if [ $1 ];then
+    for port in ${ports[@]};do
+        comprovarPort $1 $port &
+    done
+else
+    echo -e "[!] Format incorrecte, tens que passar una ip"
+fi
+wait
